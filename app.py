d82c18ae24bee3a00b7ab7abf72e6225c9bcb991
Ollie <ollie@Ollies-MBP.home>
2021-05-19 00:45:41 +0100

    Complete CRUD flask, sqlite, python template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///test.db'
-db= SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,9 +14,48 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/', methods=['get', 'post'])
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return  render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding a task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return  render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task"
+
+@app.route('/update/<int:id>', methods = ['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue updating thee task"
+    else:
+        return  render_template('index.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
